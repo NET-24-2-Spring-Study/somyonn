96376163d8f5d6845bf9cc6024a46cc385691a1c
somyonn <turfksm@gmail.com>
2024-09-19 19:45:06 +0900

    주문과 할인 도메인 구현

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,35 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
 public class MemberApp {
 
+//    public static void main(String[] args) {
+//        MemberService memberService = new MemberServiceImpl();
+//        Member member = new Member(1L, "memberA", Grade.VIP);
+//        memberService.join(member);
+//
+//        Member findMember = memberService.findMember(1L);
+//        System.out.println("new member = " + member.getName());
+//        System.out.println("findMember = " + findMember.getName());
+//    }
+
     public static void main(String[] args) {
         MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        OrderService orderService = new OrderServiceImpl();
+
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member.getName());
-        System.out.println("findMember = " + findMember.getName());
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
+
     }
 }
