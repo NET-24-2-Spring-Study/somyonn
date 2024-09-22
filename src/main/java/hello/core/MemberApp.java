a3fc07e6db237b387e7d214b1aa0d0e6be9a756d
somyonn <turfksm@gmail.com>
2024-09-22 20:50:46 +0900

    AppConfig.java <- 새로운 구조, 역할 간의 의존관계를 주입해주는 역할을 만들었다. 관심사의 분리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -11,19 +11,26 @@ import hello.core.order.OrderServiceImpl;
 public class MemberApp {
 
 //    public static void main(String[] args) {
-//        MemberService memberService = new MemberServiceImpl();
+//
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        Member member = new Member(1L, "memberA", Grade.VIP);
 //        memberService.join(member);
 //
 //        Member findMember = memberService.findMember(1L);
 //        System.out.println("new member = " + member.getName());
 //        System.out.println("findMember = " + findMember.getName());
+//
 //    }
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
 
+//      MemberService memberService = new MemberServiceImpl();
+//      OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
