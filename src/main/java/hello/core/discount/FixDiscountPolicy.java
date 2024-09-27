d3e446da554d192331040aecb1009e9382d361fe
somyonn <turfksm@gmail.com>
2024-09-27 10:37:26 +0900

    스프링 빈 생명주기 콜백

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,12 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
